46f2f9984a9cf0e2072c6a315440dd99dda67300
Kamran Payne <mail@kamranpayne.com>
2014-11-29 20:38:15 -0500

    created jekyll plugin

---
+++ _plugins/tags.rb
@@ -0,0 +1,15 @@
+module Jekyll
+	class RenderLine < Liquid::Tag
+
+		def initialize(tag_name, filename, tokens)
+			super
+			@text = File.readlines("bio.txt").sample
+		end
+
+		def render(context)
+			"#{@text}"
+		end
+	end
+end
+
+Liquid::Template.register_tag('render_line', Jekyll::RenderLine)
